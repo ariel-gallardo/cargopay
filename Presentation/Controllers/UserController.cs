b6d8ea7b44e5202b1fc0f246089d2bc48f2de4d1
Ariel Gallardo <ariel.gallardo.dev@gmail.com>
2025-01-31 16:22:04 -0300

    main# App Initial

---
+++ Presentation/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpPost]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
